265b68b8556a9a2df811bd184bb35e1f9d465bc2
gaoyang <232886019@qq.com>
2015-07-28 22:50:20 +0800

    提交

--- workFaceSurvey/WebRoot/jsp/website/regist.jsp
+++ workFaceSurvey/WebRoot/jsp/website/regist.jsp
@@ -1,12 +1,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
-<%
-	request.setAttribute("contextPath", request.getContextPath());
-	String webRoot = "http://" + request.getServerName() + ":"
-			+ request.getServerPort() + request.getContextPath();
-%>
 <html>
 <head>
+   <%@ include file="../commonincludes.jsp"%>
 	<meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -24,7 +20,41 @@
 		
 		<!-- 最新的 Bootstrap 核心 JavaScript 文件 -->
 		<script src="//cdn.bootcss.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="<%=webRoot %>/js/jquery-1.11.1.js"></script>
+        <SCRIPT src="<%=webRoot %>/js/imagecropper.min.js" type="text/javascript"></SCRIPT>
 		<script type="text/javascript">
+
+
+		var cropper;
+
+		function init()
+		{	
+			cropper = new ImageCropper(200, 200, 180, 180);
+			cropper.setCanvas("cropper");
+
+			if(!cropper.isAvaiable())
+			{
+				alert("Sorry, your browser doesn't support FileReader, please use Firefox3.6+ or Chrome10+ to run it.");
+			}
+		}
+
+		function selectImage(fileList)
+		{
+			cropper.loadImage(fileList[0]);
+			$("#imgbt1").hide();
+			$("#imgbt2").show();
+		}
+
+		
+
+		function getdata(){
+			var imgData = cropper.getCroppedImageData(180, 180);
+			$("#tupian").attr('src',imgData); 
+			$("#tp").val(imgData); 
+			$("#imgbt2").hide();
+			$("#imgbt1").show();
+		}
+		
 		function validate(){
 			var reg= new RegExp( "^0?(13|15|18|14|17)[0-9]{9}$");
 			var phone=$("#inputTelNum").val();
@@ -55,19 +85,22 @@
 		</script>
 </head>
 
-<body class="demo1">
+<body class="demo1" onload="init()">
 		<div class="main">
 			<section class="page1">
 		      	<div class="page_container">
-		        	<div >
-		        		<img class='logo' style="padding-top:0px;box-sizing:content-box" alt="workface" src="<%=webRoot%>/images/logo.png">
-		        	</diV>
-		        	<div class="title" style="padding-top: 16px;font-weight:bold">
-		        		创&nbsp;业&nbsp;上&nbsp;海&nbsp;万&nbsp;人&nbsp;田&nbsp;野&nbsp;调&nbsp;查&nbsp;&nbsp;
-		        	</div>
+		        	
 					<div class="container">
 						<form class="form-signin" action="<%=webRoot %>/survey/saveregist" method="post">
-							<h2 class="form-signin-heading">Please register</h2>
+							<DIV id="container" class="thumbnail" >
+							
+		 <INPUT name="input" id="input" style="visibility: hidden;" onchange="selectImage(this.files)" type="file" size="10">
+<DIV id="wrapper"><CANVAS id="cropper"></CANVAS><SPAN id="status" 
+style="left: 350px; top: 10px; width: 100px; position: absolute;"></SPAN>			 
+</DIV></DIV>
+<input type="hidden" name="tp" id="tp" />
+       <input id="imgbt1" class="btn btn-lg btn-primary btn-block" onclick="document.getElementById('input').click();"   value="选择图片" type="button"/>
+       <input id="imgbt2" class="btn btn-lg btn-primary btn-block" value="保存图片" style="display:none"  onclick="getdata();" type="button"/>
 							<label class="sr-only" for="inputTelNum">Phone Number</label>
 							<input id="inputTelNum" class="form-control" name="PhoneNum" type="tel" autofocus="" required="" placeholder="Phone Number">
 							<label class="sr-only" for="inputPassword">Password</label>
--- workFaceSurvey/src/com/web/controller/employee/surveyController.java
+++ workFaceSurvey/src/com/web/controller/employee/surveyController.java
@@ -1,5 +1,7 @@
 package com.web.controller.employee;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.List;
@@ -8,11 +10,18 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import sun.misc.BASE64Decoder;
+
+import com.qiniu.common.QiniuException;
+import com.qiniu.http.Response;
+import com.qiniu.storage.UploadManager;
+import com.qiniu.util.Auth;
 import com.web.common.utils.StringUtil;
 import com.web.entity.User;
 import com.web.service.ISurveyService;
@@ -23,94 +32,107 @@ import com.web.service.ISynthesizeService;
 public class surveyController {
 	@Autowired
 	ISurveyService surveyServey;
-	
+
 	@Autowired
 	public ISynthesizeService synthesizeService;
-	
-	@RequestMapping(value="/baseInfo",method=RequestMethod.POST)
-	public String  baseInfo(HttpServletRequest request,HttpServletResponse response){
+
+	@RequestMapping(value = "/baseInfo", method = RequestMethod.POST)
+	public String baseInfo(HttpServletRequest request,
+			HttpServletResponse response) {
 		return "/jsp/website/survey";
 	}
-	
-	@RequestMapping(value="/savebaseInfo",method=RequestMethod.POST)
-	public String  savebaseInfo(HttpServletRequest request,HttpServletResponse response){
-		String phone=request.getParameter("");
-		Enumeration rnames=request.getParameterNames();
-		for (Enumeration e = rnames ; e.hasMoreElements() ;) {
-		         String thisName=e.nextElement().toString();
-		        String thisValue=request.getParameter(thisName);
-//		        System.out.println(thisName+"-------"+thisValue);
+
+	@RequestMapping(value = "/savebaseInfo", method = RequestMethod.POST)
+	public String savebaseInfo(HttpServletRequest request,
+			HttpServletResponse response) {
+		String phone = request.getParameter("");
+		Enumeration rnames = request.getParameterNames();
+		for (Enumeration e = rnames; e.hasMoreElements();) {
+			String thisName = e.nextElement().toString();
+			String thisValue = request.getParameter(thisName);
+			// System.out.println(thisName+"-------"+thisValue);
 		}
 		return "";
 	}
-	
-	@RequestMapping(value="/login",method=RequestMethod.POST)
-	public String  login(HttpServletRequest request,HttpServletResponse response){
-		String phone=request.getParameter("phoneNum");
-		String password=request.getParameter("password");
-		if(!"".equals(phone) && !"".equals(password) && phone!=null && password!=null){
-			User user=surveyServey.checkLogin(phone,password);
-			if(null!=user){
+
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String login(HttpServletRequest request, HttpServletResponse response) {
+		String phone = request.getParameter("phoneNum");
+		String password = request.getParameter("password");
+		if (!"".equals(phone) && !"".equals(password) && phone != null
+				&& password != null) {
+			User user = surveyServey.checkLogin(phone, password);
+			if (null != user) {
 				request.getSession().setAttribute("username", user.getPhone());
 				request.getSession().setAttribute("phone", user.getPhone());
 				request.getSession().setAttribute("id", user.getId());
 				request.getSession().setAttribute("role", user.getRole());
 
-				if(user.getRole().equals("1")){
-//					return "/jsp/website/choose";
+				if (user.getRole().equals("1")) {
+					// return "/jsp/website/choose";
 					return "redirect:/survey/showmanage";
 
-				}else{
-//					return "/jsp/website/wenjuan1";
-					//在Session中保存手机号码
+				} else {
+					// return "/jsp/website/wenjuan1";
+					// 在Session中保存手机号码
 					return "redirect:/synthesize/questionnaire1";
 				}
-			}else{
+			} else {
 				request.setAttribute("error", "用户名或密码错误，请重新输入");
 				return "/jsp/website/login";
 			}
-		}else{
+		} else {
 			return "/jsp/website/login";
 		}
 	}
-	
+
 	@RequestMapping("/choose")
-	public String  choose(HttpServletRequest request,HttpServletResponse response){
+	public String choose(HttpServletRequest request,
+			HttpServletResponse response) {
 		return "/jsp/website/choose";
 	}
-	
+
 	@RequestMapping("/regist")
-	public String  regist(HttpServletRequest request,HttpServletResponse response){
+	public String regist(HttpServletRequest request,
+			HttpServletResponse response) {
 		return "/jsp/website/regist";
 	}
-	
-	@RequestMapping(value="/saveregist",method=RequestMethod.POST)
-	public String  saveregist(HttpServletRequest request,HttpServletResponse response){
-		String phone=request.getParameter("PhoneNum");
-		String password=request.getParameter("password");
-		String passwordRpe=request.getParameter("passwordRpe");
-		if(!"".equals(phone) && !"".equals(password) && password.equals(passwordRpe) ){
-			User user=surveyServey.getUserByPhone(phone);
-			if(null!=user){
+
+	@RequestMapping(value = "/saveregist", method = RequestMethod.POST)
+	public String saveregist(HttpServletRequest request,
+			HttpServletResponse response) {
+		String phone = request.getParameter("PhoneNum");
+		String password = request.getParameter("password");
+		String passwordRpe = request.getParameter("passwordRpe");
+		String tp = StringUtil.safeToString("tp", "");
+		if (!"".equals(phone) && !"".equals(password)
+				&& password.equals(passwordRpe)) {
+			User user = surveyServey.getUserByPhone(phone);
+			if (null != user) {
 				request.setAttribute("error", "该手机号已被注册，请重试");
 				return "/jsp/website/regist";
-			}else{
-				user=new User();
+			} else {
+
+				uploadimg(tp,phone+"_img.jpg");
+
+				user = new User();
 				user.setPhone(phone);
 				user.setPassword(password);
 				user.setRole("2");
+				
 				surveyServey.saveUser(user);
 				return "/jsp/website/login";
-	
+
 			}
-		}else{
+		} else {
 			request.setAttribute("error", "请确保密码、用户名输入正确");
 			return "/jsp/website/regist";
 		}
 	}
-	
+
 	@RequestMapping("/showmanage")
-	public String  showmangee(HttpServletRequest request,HttpServletResponse response){
+	public String showmangee(HttpServletRequest request,
+			HttpServletResponse response) {
 		Map map = new HashMap();
 		String page = StringUtil
 				.safeToString(request.getParameter("page"), "1");
@@ -134,4 +156,52 @@ public class surveyController {
 		return "/jsp/website/manage";
 	}
 
+	String ACCESS_KEY = "ArTWIe1q_1iDUdMm2notK3vjhARjbcNa_8S1zrZ5";
+	String SECRET_KEY = "N9fwW3xXTNvUCdBKQqtcYZsUmaDquwSK1xh9anFv";
+
+	Auth auth = Auth.create(ACCESS_KEY, SECRET_KEY);
+
+	private String getUpToken0(String key) {
+		// return auth.uploadToken("wf-photos", key);
+		String token = auth.uploadToken("wf-photos", key);
+		System.out.println(token);
+		return token;
+	}
+
+	private void uploadimg(String img, String key) {
+		UploadManager uploadManager = new UploadManager();
+		try {
+
+			Response res = uploadManager.put(decode(img
+					.substring("data:image/jpeg;base64,".length())), key,
+					getUpToken0(key));
+
+			Map<String, Object> m4 = res.jsonToObject(HashMap.class);
+
+		} catch (QiniuException e) {
+			Response r = e.response;
+			System.out.println(r.toString());
+			try {
+				System.out.println(r.bodyString());
+			} catch (QiniuException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	private boolean saveImageToDisk(byte[] data, String imageName)
+			throws IOException {
+		FileUtils.writeByteArrayToFile(new File(imageName), data);
+
+		return true;
+	}
+
+	private byte[] decode(String imageData) throws IOException {
+		BASE64Decoder decoder = new BASE64Decoder();
+		byte[] data = decoder.decodeBuffer(imageData);
+		return data;
+	}
 }
